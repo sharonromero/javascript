849f375608bd7302187fa379d287f9c5f5045590
sharonromero <sh-aron@hotmail.com>
2019-02-12 16:28:10 -0700

    Wrote some javascript in a separate file

---
+++ script.js
@@ -0,0 +1,4 @@
+//This asks the user for his name and stores it in a variable, then alerts nice to meet you and the userName in a pop-up box, then it prints the great to meet you message plus the userName in the console.  
+var userName = prompt("What is your name?");
+alert("Nice to meet you, " + userName);
+console.log("Also great to meet you, " + userName);
