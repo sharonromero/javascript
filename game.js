73967032e7e910f23baa395299ba357a09fca34f
sharonromero <sh-aron@hotmail.com>
2019-02-14 17:06:45 -0700

    Added JS for printing each character in a string and While Loops Problem Set

--- game.js
+++ game.js
@@ -26,3 +26,91 @@ else if(Number(guess) > secretNumber) {
 else {
 	alert("Too low. Guess again!");
 }
+
+//While Loops (not part of Guessing Game) - Printing each character in a string.
+//string we're looping over:
+// var str = "hello";
+//first character is at index 0
+// var count = 0;
+
+// while(count < str.length) {
+// 	console.log(str[count]);
+// 	count++;
+// }
+
+//WHILE LOOPS PROBLEM SET:
+//Print all numbers between -10 and 19
+var num = -10
+
+while(num >= -10) {
+	if(num <= 19) {
+		console.log(num);	
+	}
+	num++;
+}
+//His code for above:
+//var counter = -10;
+//while(counter < 20) {
+	//console.log(counter);
+	//counter++;
+//}
+
+//Print all even numbers between 10 and 40
+var num = 10
+
+while(num <= 40) {
+		console.log(num + 1);	
+		num++;
+}
+//His code for above (two different ways):
+//1st way:
+//var counter = 10;
+
+//while(counter <= 40) {
+	//console.log(counter);
+	//counter+=2;
+//}
+//2nd way:
+//var counter = 10;
+
+//while(counter <= 40) {
+	//if(counter % 2 === 0) {
+		//console.log(counter);
+	//}
+	//counter+=1;
+//}
+
+//Print all odd numbers between 300 and 333
+
+var num = 300
+
+while(num <= 333); {
+	console.log(num +3);
+	num++;
+}
+//His code for above:
+//var counter = 300;
+//while(counter <= 333) {
+	//if(counter % 2 !==0) {
+		//console.log(counter);
+	//}
+	//counter+=1;
+//}
+
+//Print all numbers divisible by 5 AND 3 between 5 and 50
+
+var num = 5
+
+while (num = 5); {
+	console.log(num ())
+}
+
+//His code for above:
+//var counter = 5;
+
+//while(counter <= 50) {
+	//if(counter % 5 === 0 && counter % 3 === 0) {
+		//console.log(counter);
+	//}
+	//counter+=1;
+//}
