d6e691f253031a5ce3db3a6f6b5a488ff67dbbb1
sharonromero <sh-aron@hotmail.com>
2019-02-19 13:43:30 -0700

    Added code to add new todo, list todos, and quit to do list.

--- list.js
+++ list.js
@@ -1,7 +1,18 @@
-var todos = [];
+var todos = ["Buy new turtle"];
 
 var input = prompt("What would you like to do?");
 
-if(input === "list") {
-	console.log(todos);
+while(input !== "quit") {
+	//handle input
+	if(input === "list") {
+		console.log(todos);
+	} else if(input === "new") {
+		//ask for new todo
+		var newTodo = prompt("Enter new todo");
+		//add to todos array
+		todos.push(newTodo);
 }
+	//ask again for new input
+	input = prompt("What would you like to do?");
+}
+console.log("Okay, you quit the app");
