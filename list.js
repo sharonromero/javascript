8165f6168874cddd2baf412ea5fe0eba9159cd91
sharonromero <sh-aron@hotmail.com>
2019-02-19 16:06:59 -0700

    Added delete to todo list, cleaned up js

--- list.js
+++ list.js
@@ -5,14 +5,38 @@ var input = prompt("What would you like to do?");
 while(input !== "quit") {
 	//handle input
 	if(input === "list") {
-		console.log(todos);
+		listTodos();
 	} else if(input === "new") {
-		//ask for new todo
-		var newTodo = prompt("Enter new todo");
-		//add to todos array
-		todos.push(newTodo);
-}
+		addTodo();
+	} else if(input === "delete") {
+		deleteTodo();
+	}
 	//ask again for new input
 	input = prompt("What would you like to do?");
 }
 console.log("Okay, you quit the app");
+
+function listTodos() {
+	console.log("**********")
+		todos.forEach(function(todo, index) {
+			console.log(index + ": " + todo);
+		});
+		console.log("**********")
+}
+
+function addTodo() {
+	//ask for new todo
+	var newTodo = prompt("Enter new todo");
+	//add to todos array
+	todos.push(newTodo);
+	console.log("Added Todo");
+}
+
+function deleteTodo() {
+	//ask for index of todo to be deleted
+	var index = prompt("Enter index of todo to delete");
+	//delete that todo
+	//splice()
+	todos.splice(index, 1);
+	console.log("Deleted Todo");
+}
